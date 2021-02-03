8f77c39e1ca9171e99d5a0fc8cdd92bf7ec8633a
nathan hains <nathanhains14@yahoo.com>
2021-02-03 16:33:50 -0500

    commit tree ea802a400f01072c10a4e24f0b0a147c9cf85c07

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+        if array.size == 1
+            array.join
+        elsif array.size == 2
+            array.join(" and ")
+        elsif array.size >= 3
+            new_array = array
+            star = new_array.pop
+            new_array.join
+            new_array << "and "
+            new_array.join(", ") + star
+    end 
 end
